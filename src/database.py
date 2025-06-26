2eeabd60542602fc856c5eb8b45e46634d3114a9
Torben Kahnert <torben@tmak2002.dev>
2025-06-26 14:45:05 +0200

    more env

--- src/database.py
+++ src/database.py
@@ -4,8 +4,9 @@ from sqlmodel import create_engine, Session
 
 user = os.getenv("dbuser")
 password = os.getenv("dbpass")
+url = os.getenv("dburl")
 
-DATABASE_URL = f"postgresql://{user}:{password}@postgres:5432/users"
+DATABASE_URL = f"postgresql://{user}:{password}@{url}:5432/users"
 engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
