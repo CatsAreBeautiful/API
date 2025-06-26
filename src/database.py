366431dc15a6f4cb36c9e36ecbb32e78049ec254
Torben Kahnert <torben@tmak2002.dev>
2025-06-26 14:17:40 +0200

    we use env vars

--- src/database.py
+++ src/database.py
@@ -1,6 +1,11 @@
+import os
 from sqlmodel import create_engine, Session
 
-DATABASE_URL = "postgresql://api:api@postgres:5432/users"
+
+user = os.getenv("dbuser")
+password = os.getenv("dbpass")
+
+DATABASE_URL = f"postgresql://{user}:{password}@postgres:5432/users"
 engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
