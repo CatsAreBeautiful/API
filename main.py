c5504e2f192de409fbbf90e62a67c6526ba3ef9c
Torben Kahnert <torben@tmak2002.dev>
2025-08-24 14:00:19 +0200

    Add basic add user and del user logic
    Signed-off-by: Torben Kahnert <torben@tmak2002.dev>

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
-def main():
-    print("Hello from api!")
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, HTTPException
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
+class Users(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
 
-if __name__ == "__main__":
-    main()
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Start App")
+    SQLModel.metadata.create_all(engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/users/add/")
+async def add_user(name: str):
+    with Session(engine) as session:
+        statement = select(Users).where(Users.name == name)
+        # results = session.exec(statement)
+        if session.exec(statement).first() is None:
+            session.add(Users(name=name))
+            session.commit()
+            return {"message": f"User {name} sucessfully added!"}
+        else:
+            raise HTTPException(status_code=404, detail="User already exists!")
+
+@app.delete("/users/del")
+async def del_user(name: str):
+    with Session(engine) as session:
+        statement = select(Users).where(Users.name == name)
+        # results = session.exec(statement)
+        if session.exec(statement).first() is None:
+            raise HTTPException(status_code=404, detail="User not exists!")
+        else:
+            user = session.query(Users).filter_by(name=name).one()
+            session.delete(user)
+            session.commit()
+            return {"message": f"User {name} sucessfully deleted!"}
